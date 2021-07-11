f06410be1f86764d561d6f8cf6afa372ff748ed4
Ben Bauer <ben.bauer1@gmail.com>
2021-07-10 20:14:33 -0500

    added average calculator

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ if(zeroOrOne == 0):
     print("heads")
 else:
     print("tails")
+
+student_height = [123, 123, 151, 124]
+sum = 0
+
+for height in student_height:
+    sum = sum + height
+
+print(sum/len(student_height))
